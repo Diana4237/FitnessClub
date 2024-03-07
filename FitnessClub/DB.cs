d8cbc381c91454c7407ded67038f1e19e3b33911
Diana <diana.zaharova2605@gmail.com>
2024-03-07 23:50:24 +0300

    Добавьте файлы проекта.

---
+++ FitnessClub/DB.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessClub
+{
+    public class DB
+    {
+        SqlConnection sqlconnection = new SqlConnection("Data Source=-PC\\MSSQLSERVER01; Initial Catalog=FitnessClub;Integrated Security=True;");
+
+        public void OpenConnection()
+        {
+            if (sqlconnection.State == System.Data.ConnectionState.Closed)
+            {
+                sqlconnection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (sqlconnection.State == System.Data.ConnectionState.Open)
+            {
+                sqlconnection.Close();
+            }
+        }
+
+        public SqlConnection getConnection()
+        {
+            return sqlconnection;
+        }
+
+    }
+}
---
+++ FitnessClub/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FitnessClub
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string connectionString = @"Data Source=-PC\MSSQLSERVER01; Initial Catalog=FitnessClub;Integrated Security=True;";
+        public MainWindow()
+        {
+            InitializeComponent();
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            string cmd = $"SELECT * FROM Client";
+            SqlCommand createCommand = new SqlCommand(cmd, connection);
+            createCommand.ExecuteNonQuery();
+            SqlDataAdapter dataAdp = new SqlDataAdapter(createCommand);
+            DataTable dt = new DataTable("Client");
+            dataAdp.Fill(dt);
+           // datagrid.ItemsSource = dt.DefaultView;
+            connection.Close();
+
+        }
+    }
+}
